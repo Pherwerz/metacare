0a23e5710a730b5159912a9f21e8c254580f7408
algorithm <lordrisk@gmail.com>
2022-03-25 10:30:58 +0100

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline bg-red-900">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
